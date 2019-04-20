cf70725ba63d3fb1d3a64b9734e2a1ab5e4e6e6b
AliAnilKocak <alianilkocak@gmail.com>
2019-04-21 01:02:21 +0300

    Update MainActivity.java
    Entity created
    Dao & Room Database
    Repository class created
    ViewModel class created

--- app/src/main/java/com/cale/notesmvvm/notesappmvvm/MainActivity.java
+++ app/src/main/java/com/cale/notesmvvm/notesappmvvm/MainActivity.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    
     private NoteViewMoel noteViewMoel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
